2784f79e7dd537fd32f7c184a4434e008d1855b7
Buyoung Chung <56213742+qndud0206@users.noreply.github.com>
2022-07-11 23:33:48 +0900

    중간

--- src/App.js
+++ src/App.js
@@ -4,12 +4,44 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import Home from "./pages/Home";
-
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+//process.env.PUBLIC_URL public 폴더의 경로를 바로 쓸 수 있는 명령어
+//<img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton
+              text={"왼쪽 버튼"}
+              onClick={() => alert("click!")}
+              type={"positive"}
+            />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("click!")}
+              type={"negative"}
+            />
+          }
+        />
         <h2>App.js</h2>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("click!")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("click!")}
+          type={"negative"}
+        />
+        <MyButton text={"버튼"} onClick={() => alert("click!")} />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/new" element={<New />} />
