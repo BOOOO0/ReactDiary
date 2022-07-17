92953157e08a056c9a30a75b3e1a1e2179da044f
Buyoung Chung <56213742+qndud0206@users.noreply.github.com>
2022-07-17 23:11:52 +0900

    Home

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,10 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import DiaryItem from "./DiaryItem";
+import MyButton from "./MyButton";
 
-const DiaryList = ({ diaryList }) => {
+const sortOptionList = [
+  {
+    value: "latest",
+    name: "최신순",
+  },
+  { value: "oldest", name: "오래된순" },
+];
+const filterOptionList = [
+  { value: "all", name: "전부다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안좋은 감정만" },
+];
+const ControlMenu = ({ value, onChange, optionList }) => {
   return (
-    <div>
-      {diaryList.map((it) => (
-        <div key={it.id}>{it.content}</div>
+    <select
+      className="ControlMenu"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState("lastest");
+  const [filter, setFilter] = useState("all");
+  const getProcessedDiaryList = () => {
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+    // 배열을 JSON 문자화하고 그것을 다시 parse로 배열로 복호화
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    // 객체의 배열을 정렬할때는 비교함수를 작성해서 정렬해야한다
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
+    const sortedList = filteredList.sort(compare);
+    return sortedList;
+  };
+  const navigate = useNavigate();
+  return (
+    <div className="DiaryList">
+      <div className="menu_wrapper">
+        <div className="left_col">
+          <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+          />
+          <ControlMenu
+            value={filter}
+            onChange={setFilter}
+            optionList={filterOptionList}
+          />
+        </div>
+        <div className="right_col">
+          <MyButton
+            type={"positive"}
+            text={"새 일기쓰기"}
+            onClick={() => navigate("/new")}
+          />
+        </div>
+      </div>
+      {getProcessedDiaryList().map((it) => (
+        <DiaryItem key={it.id} {...it} />
       ))}
     </div>
   );
